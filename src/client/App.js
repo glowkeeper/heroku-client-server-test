77e83c8f52ae2dd32474ab5ed2db096db513c37e
glowkeeper <steve@glowkeeper.uk>
2022-03-05 13:30:10 +0000

    change example string

--- src/client/App.js
+++ src/client/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload your super awesome app.
+          Edit <code>src/App.js</code> and save to reload your awesome app.
         </p>
         <a
           className="App-link"
