2cd7f25fe2c66129c01d9be33f10a0dd6a6d51a9
glowkeeper <steve@glowkeeper.uk>
2022-03-05 13:25:05 +0000

    deploy test

--- src/client/App.js
+++ src/client/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload your really awesome app.
+          Edit <code>src/App.js</code> and save to reload your super awesome app.
         </p>
         <a
           className="App-link"
